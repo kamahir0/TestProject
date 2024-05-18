e3bc8e32c0702f15997d395db8655ca8bdb4213f
kamahir0 <hrtk1330@gmail.com>
2024-05-18 23:27:43 +0900

    UniTaskでDOTweenを待機してみた

--- Assets/Temp.cs
+++ Assets/Temp.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class Temp : MonoBehaviour
 {
-    int test = 0;
-
     UniTask uniTask;
     Tween tween;
 
     private void Start()
     {
         Debug.Log("test");
+        Test().Forget();
+    }
+
+    private async UniTask Test()
+    {
+        await transform.DOMove(Vector3.zero, 1f);
+        await UniTask.Delay(1000);
+        Debug.Log("test2");
     }
 }
