1b113d25a34693bb4a676c00d2c13e37d8b97d70
kamahir0 <hrtk1330@gmail.com>
2024-05-18 22:30:22 +0900

    UniTask導入

--- Assets/Temp.cs
+++ Assets/Temp.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Cysharp.Threading.Tasks;
 
 public class Temp : MonoBehaviour
 {
     int test = 0;
+
+    UniTask uniTask;
 
     private void Start()
     {
