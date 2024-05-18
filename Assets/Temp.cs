6de58032a02dd573ef00d438558b20e9df08e032
kamahir0 <hrtk1330@gmail.com>
2024-05-18 22:38:18 +0900

    DOTWeen導入

--- Assets/Temp.cs
+++ Assets/Temp.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 
 public class Temp : MonoBehaviour
 {
     int test = 0;
 
     UniTask uniTask;
+    Tween tween;
 
     private void Start()
     {
